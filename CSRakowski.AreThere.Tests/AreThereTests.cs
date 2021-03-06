145cbe2b473c89fc7d522a688d067ae5ba2808f0
Christiaan Rakowski <cs.rakowski@gmail.com>
2021-03-06 18:04:17 +0100

    Rewritten logic to be recursive and added Even/Odd

--- CSRakowski.AreThere.Tests/AreThereTests.cs
+++ CSRakowski.AreThere.Tests/AreThereTests.cs
@@ -32,6 +32,29 @@ namespace CSRakowski.AreThere.Tests
             Assert.Equal(expected, actual);
         }
 
+        [Theory]
+        [MemberData(nameof(GetData_Count))]
+        public void ThereAre_NoMoreThan<T>(IEnumerable<T> elements, int numberOfElements, bool expected)
+        {
+            var actual = ThereAre.NoMoreThan(numberOfElements, elements);
+            Assert.NotEqual(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData_Even))]
+        public void ThereAre_EvenNumberOfElements<T>(IEnumerable<T> elements, bool expected)
+        {
+            var actual = ThereAre.EvenNumberOfElements(elements);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData_Even))]
+        public void ThereAre_OddNumberOfElements<T>(IEnumerable<T> elements, bool expected)
+        {
+            var actual = ThereAre.OddNumberOfElements(elements);
+            Assert.NotEqual(expected, actual);
+        }
 
         public static IEnumerable<object[]> GetData_Any =>
             new List<object[]>
@@ -47,6 +70,8 @@ namespace CSRakowski.AreThere.Tests
             new List<object[]>
             {
                 new object[] { Enumerable.Range(1, 10), 10, true },
+                new object[] { Enumerable.Range(1, 10), 11, false },
+                new object[] { Enumerable.Range(1, 10), 9, true },
                 new object[] { Enumerable.Empty<int>(), 0, true },
                 new object[] { new[] { 1, 2, 3}, 3, true },
                 new object[] { Array.Empty<int>(), 0, true },
@@ -56,6 +81,19 @@ namespace CSRakowski.AreThere.Tests
                 new object[] { new[] { 1, 2, 3}, 4, false },
                 new object[] { Array.Empty<int>(), 1, false },
                 new object[] { new List<int>(), 1, false }
+            };
+
+        public static IEnumerable<object[]> GetData_Even =>
+            new List<object[]>
+            {
+                new object[] { Enumerable.Range(1, 10), true },
+                new object[] { Enumerable.Range(1, 4), true },
+                new object[] { Enumerable.Range(1, 5), false },
+                new object[] { Enumerable.Range(1, 9), false },
+                new object[] { Enumerable.Empty<int>(), true },
+                new object[] { new[] { 1, 2, 3}, false },
+                new object[] { Array.Empty<int>(), true },
+                new object[] { new List<int>(), true }
             };
     }
 }
--- CSRakowski.AreThere/Library.fs
+++ CSRakowski.AreThere/Library.fs
@@ -3,24 +3,51 @@
 open System.Collections.Generic
 
 module public ThereAre =
-    let Any<'T> (collection: IEnumerable<'T>): bool =
+    let Any<'T> (collection: IEnumerable<'T>) =
         collection.GetEnumerator().MoveNext()
 
-    let No<'T> (collection: IEnumerable<'T>): bool =
+    let No<'T> (collection: IEnumerable<'T>) =
         collection.GetEnumerator().MoveNext() |> not
 
-    let AtLeast<'T> (numberOfElement: int) (collection: IEnumerable<'T>): bool =
-        let mutable counted = 0;
-
-        let enum = collection.GetEnumerator()
-
-        let mutable continueLooping = true
-        while continueLooping do
-            if enum.MoveNext() |> not then
-                continueLooping <- false
+    let AtLeast<'T> (numberOfElements: int) (collection: IEnumerable<'T>) =
+        let rec _atLeast (enumerator: IEnumerator<'T>) (currentCount: int) (numberOfElements: int) =
+            if currentCount >= numberOfElements then
+                true
             else
-                counted <- counted + 1
-                if (counted >= numberOfElement) then
-                   continueLooping <- false
+                if enumerator.MoveNext() then
+                    _atLeast enumerator (currentCount + 1) numberOfElements
+                else
+                    false
 
-        counted >= numberOfElement
+        _atLeast (collection.GetEnumerator()) 0 numberOfElements
+
+
+    let NoMoreThan<'T> (numberOfElements: int) (collection: IEnumerable<'T>) =
+        let rec _noMoreThan (enumerator: IEnumerator<'T>) (currentCount: int) (numberOfElements: int) =
+            if currentCount >= numberOfElements then
+                false
+            else
+                if enumerator.MoveNext() then
+                    _noMoreThan enumerator (currentCount + 1) numberOfElements
+                else
+                    true
+
+        _noMoreThan (collection.GetEnumerator()) 0 numberOfElements
+
+    let EvenNumberOfElements<'T> (collection: IEnumerable<'T>) =
+        let rec _even (enumerator: IEnumerator<'T>) (currentCount: int) =
+            if enumerator.MoveNext() |> not then
+                currentCount % 2 = 0
+            else
+                _even enumerator (currentCount + 1)
+
+        _even (collection.GetEnumerator()) 0
+
+    let OddNumberOfElements<'T> (collection: IEnumerable<'T>) =
+        let rec _odd (enumerator: IEnumerator<'T>) (currentCount: int) =
+            if enumerator.MoveNext() |> not then
+                currentCount % 2 <> 0
+            else
+                _odd enumerator (currentCount + 1)
+
+        _odd (collection.GetEnumerator()) 0
